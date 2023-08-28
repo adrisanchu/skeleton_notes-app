0b893561d848a7e69b5338c35b30d9588937e135
adrisanchu <adrian.sanchez@ree.es>
2023-08-28 17:15:41 +0200

    #AppShell: basic setup of the layout with a header, a sidebar and a footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script lang='ts'>
+<script lang="ts">
 	// The ordering of these imports is critical to your app working properly
 	// import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	import '../theme.postcss';
@@ -6,6 +6,26 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell slotSidebarLeft="w-52 bg-surface-500/10 p-4">
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead"><strong class="text-xl uppercase">Noted</strong></svelte:fragment
+			>
+			<svelte:fragment slot="trail"
+				><Avatar initials="AS" width="w-10" background="bg-primary-500" /></svelte:fragment
+			>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<!-- Router Slot -->
+	<div class="container p-10 mx-auto">
+		<slot />
+	</div>
+
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
